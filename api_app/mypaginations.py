86e2f87df365ba6bfed7d0600109dafd545525c4
Mohammed Shadhath <mohammedshadhath7@gmail.com>
2023-11-15 14:27:36 +0530

    first commit

---
+++ api_app/mypaginations.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import LimitOffsetPagination
+
+class MyLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 5
+    limit_query_param = 'mylimit'
+    offset_query_param = 'myoffset'
+    max_limit = 6  
+    
